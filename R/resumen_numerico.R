c4b178fb11e99a7f8d170bafb3186067512d2782
Juan Martin Bongiovanni <jmbongiovanni@mail.austral.edu.ar>
2025-11-26 16:30:22 -0300

    Documentacion de la funcion resumen_numerico

--- R/resumen_numerico.R
+++ R/resumen_numerico.R
@@ -1,3 +1,17 @@
+#' Resumen Numerico
+#'
+#' La funcion resumen_numerico sirve para obtener una tabla o tibble con el maximo, minimo, promedio,
+#' desvio estandar y mediana de una columna del archvo que se le pasa como argumento
+#'
+#' @param datos Archivo de datos
+#' @param columna Columna de de datos de tipo numerico de la cual se quiere obtenr un resumen numerico
+#'
+#' @examples
+#' resumen_nuemrico(datos1, Cantidad_de_aviones)
+#'
+#' @returns Devuelve un tibble o tabla con el maximo, minimo, promedio,
+#' desvio estandar y mediana de una columna del archvo que se le pasa como argumento
+#' @export
 resumen_numerico <- function(datos,columna) {
     if(!is.numeric(columna)) {
       cli::cli_inform( "i" = "La columna no es de tipo numerico")
